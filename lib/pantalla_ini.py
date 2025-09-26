24d0bce61f5ca6d79901693076042272a8c727c9
Javier Vivas <jrvivas@gmail.com>
2025-09-25 21:02:47 -0300

    Moduficacion de estrustura, pantalla inicio y algunas referencias

---
+++ lib/pantalla_ini.py
@@ -0,0 +1,17 @@
+#Diseño de MENU
+
+print("  ____           _   _                _              _        ")
+print(" / ___| ___  ___| |_(_) ___  _ __    / \\   _ __   __| | ___   ")
+print("| |  _ / _ \\/ __| __| |/ _ \\| '_ \\  / _ \\ | '_ \\ / _` |/ _ \\  ")
+print("| |_| |  __/\\__ \\ |_| | (_) | | | |/ ___ \\| | | | (_| | (_) | ")
+print(" \\____|\\___||___/\\__|_|\\___/|_| |_/_/   \\_\\_| |_|\\__,_|\\___/  ")
+print("                                                              ")
+print("\n\n", "\t"*3,"██████ MENU PRINCIPAL █████\n\n")
+print("\t"*3,"1) CREAR NUEVO PROYECTO")
+print("\t"*3,"2) MODIFICAR PROYECTO EXISTENTE")
+print("\t"*3,"0) Para salir")
+
+opcion=input("\n\n\t"*1+"Elija una Opción :")
+
+
+
