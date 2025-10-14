a884a2cb51ef7df3bc8ddc3ff1fc609eac2082e9
Javier Vivas <jrvs@gmail.com>
2025-10-14 20:52:36 -0300

    Definición de flujo de ventanas

--- lib/pantalla_ini.py
+++ lib/pantalla_ini.py
@@ -7,11 +7,15 @@ print("| |_| |  __/\\__ \\ |_| | (_) | | | |/ ___ \\| | | | (_| | (_) | ")
 print(" \\____|\\___||___/\\__|_|\\___/|_| |_/_/   \\_\\_| |_|\\__,_|\\___/  ")
 print("                                                              ")
 print("\n\n", "\t"*3,"██████ MENU PRINCIPAL █████\n\n")
+print("\t"*3,"1) LISTAR PROYECTO")
 print("\t"*3,"1) CREAR NUEVO PROYECTO")
 print("\t"*3,"2) MODIFICAR PROYECTO EXISTENTE")
 print("\t"*3,"0) Para salir")
 
 opcion=input("\n\n\t"*1+"Elija una Opción :")
+
+#ejecutar_opcion(opcion)
+
 
 
 
